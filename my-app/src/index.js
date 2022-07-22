b050231de5e5261fd4c3510008b6c6991f71fb24
Jonathan Biasella <jonathanbiasella@gmail.com>
2022-07-22 11:34:46 +0200

    Update Index.js

---
+++ my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import App from "./App";
+
+
+
+render default class Index extends React.Component{
+    render(){
+        return (
+            <div>
+                <App />
+            </div>
+        )
+    }
+}
