95d486c90125d66f4ebe9f2669063eb94967531a
Jonathan Biasella <jonathanbiasella@gmail.com>
2022-07-31 18:33:27 +0200

    Update TodoList.js

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,34 +1,35 @@
 import React from "react";
 
-export class TodoList extends React.Component{
+export class TodoList extends React.Component {
 
-  state ={
+  state = {
     items: ['Baku', 'Elio', 'Karim', 'Martina'],
     name: ""
-    }
+  }
 
-    handleControlledNameChange = (event) =>{
-        this.setState ({
-            name: event.target.value
-        })
-        return console.log(this.state)
-    }
+  handleControlledNameChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+    return console.log(this.state)
+  }
 
-    addElementToList = (event) =>{
-        event.preventDefault() 
+  addElementToList = (event) => {
+    event.preventDefault()
 
-        this.setState({
-            items: [...this.state.items, this.state.name]
-        })
-    }
+    this.setState({
+      items: [...this.state.items, this.state.name],
+      name: ''
+    })
+  }
 
-    render(){
-        return (<div>
-        <ul>{this.state.items.map((item, index)=> <li key={index}>{item}</li>)}</ul>
-        <form onSubmit={this.addElementToList}>
-        <input type="text" name ="newItem" value={this.state.name} onChange={this.handleControlledNameChange}/>
+  render() {
+    return (<div>
+      <ul>{this.state.items.map((item, index) => <li key={index}>{item}</li>)}</ul>
+      <form onSubmit={this.addElementToList}>
+        <input type="text" name="newItem" value={this.state.name} onChange={this.handleControlledNameChange} />
         <button type="submit" name="addToList">Add to list</button>
-        </form>
-        </div>)
-    }
+      </form>
+    </div>)
+  }
 }
