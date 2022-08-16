15a3625010ffce0e5c1791cbd788aca37dd5f9bb
jbiasella <jonathanbiasella@gmail.com>
2022-08-16 17:03:27 +0200

    Update Counter.js

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,25 +1,21 @@
-import { useState } from "react"
+import React, { useEffect, useState } from 'react';
 
-export function HookCounter({ initialValue = 0}){
-    
-    const [counter, setCounter] = useState(initialValue)
-    
+export function Counter({initialValue = 0, onCounterChange}){
+    const [counter, setCounter] = useState(initialValue);
+
+    useEffect(()=>{onCounterChange(counter)}, [counter,onCounterChange])
+
     function handleCounterIncrement(){
         setCounter(c => c + 1)
     }
 
-    function handleCounterReset() {
+    function handleCounterReset(){
         setCounter(initialValue)
     }
-    
-    return(
-        <>
-        
-        <h2>Counter: {counter} </h2>
+
+    return(<div>
+        <h1>Counter: {counter}</h1>
         <button onClick={handleCounterIncrement}>Increment</button>
         <button onClick={handleCounterReset}>Reset</button>
-        </>
-
-
-    )
+        </div>)
 }
