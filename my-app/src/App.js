ce47030a2d2dd349a2ac246c4e12e7e01ea663e8
jbiasella <jonathanbiasella@gmail.com>
2022-08-20 12:02:49 +0200

    sad

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, from "react";
 import { Welcome } from "./Welcome";
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom';
 
 
 export function App() {
--- my-app/src/Root.jsx
+++ my-app/src/Root.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter} from 'react-router-dom'
-import { App } from './app'
+import { App } from './App'
 
 export function Root(){
  return(
