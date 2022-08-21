6f03e76ca4d11548b50be4bd33ada9022d72b407
jbiasella <jonathanbiasella@gmail.com>
2022-08-21 22:43:31 +0200

    Update App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
+import { Counter } from "./Counter";
+import { ShowGithubUser } from "./ShowGithubUser";
 import { Welcome } from "./Welcome";
 
 export function App() {
   return (
     <div>
-    <h1>My awesome app!</h1>
-    <Link to="/">Home</Link>
-    <Link to="/users/:username">githubUser</Link>
-  <Routes>
-    <Route path="/" element={<Welcome name="Erik"/>} />
-    <Route path="users/:username" element={<ShowGithubUser />} />
-  </Routes>
-</div>  );
+        <h1>My awesome app!</h1>
+        <Link to="/">Home</Link>
+        <Link to="/users/:username">githubUser</Link>
+        <Link to="/counter">counter</Link>
+      <Routes>
+        <Route path="/" element={<Welcome name="Erik"/>} />
+        <Route path="users/:username" element={<ShowGithubUser />} />
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
