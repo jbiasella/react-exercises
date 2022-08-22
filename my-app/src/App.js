b8743d79c03eb055e0479c1e790ad26bd30ebbc7
Jonathan Biasella <jonathanbiasella@gmail.com>
2022-08-22 12:28:23 +0200

    Edit file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
 import { Welcome } from "./Welcome";
+import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
   return (
@@ -8,7 +10,7 @@ export function App() {
     <Link to="/">Home</Link>
     <Link to="/users/:username">githubUser</Link>
   <Routes>
-    <Route path="/" element={<Welcome name="Erik"/>} />
+    <Route path="/" element={<Welcome name="Jbiasella"/>} />
     <Route path="users/:username" element={<ShowGithubUser />} />
   </Routes>
 </div>  );
